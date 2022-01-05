8af21bac23e522792ffb58cc9778562c249de335
Goran <39734799+GoXYzz@users.noreply.github.com>
2022-01-05 21:23:24 +0100

    Update change_word.py
    One function

--- change_word.py
+++ change_word.py
@@ -1,21 +1,20 @@
-# Ucitati text
-# Uneti rec koju zelimo zameniti
-# Uneti rec zamene
-# Novi tekst sacuvati u novom fajlu
-# Stari fajl ostaje nepromenjen
-
 def char_changer():
+    # Word we want to change
     base_char = input("Enter base char: ")
+    # New word
     enc_char = input("Enter new char: ")
+    # Name of file with original text
     original_file_name = input("Enter the name of file: ")
+    # New file with changed text
     new_file_name = input("Enter the name of new file: ")
+    # Load orignal file
     with open(original_file_name) as source_file:
         lines = source_file.readlines()
-    def input_char():
-             with open(new_file_name, 'w') as output:
-                 for line in lines:
-                    new_line = line.replace(base_char, enc_char)
-                    output.write(new_line)
-    input_char()
+        # New file with changed text
+        with open(new_file_name, 'w') as output:
+            for line in lines:
+                new_line = line.replace(base_char, enc_char)
+                output.write(new_line)
+
 
 char_changer()

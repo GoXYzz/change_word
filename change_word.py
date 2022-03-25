052100864ba708443a50e46cf17d4fc71eedb3ad
Goxy <gorankalajdzic92@gmail.com>
2022-03-25 18:57:32 +0100

    testinho

--- change_word.py
+++ change_word.py
@@ -1,20 +1,28 @@
-def char_changer():
-    # Word we want to change
-    base_char = input("Enter base char: ")
-    # New word
-    enc_char = input("Enter new char: ")
-    # Name of file with original text
-    original_file_name = input("Enter the name of file: ")
-    # New file with changed text
-    new_file_name = input("Enter the name of new file: ")
-    # Load orignal file
-    with open(original_file_name) as source_file:
-        lines = source_file.readlines()
-        # New file with changed text
-        with open(new_file_name, 'w') as output:
-            for line in lines:
-                new_line = line.replace(base_char, enc_char)
-                output.write(new_line)
+# Symbol to change
+base_char = input("Enter base char: ")
+# New symbol
+new_char = input("Enter new char: ")
+# Name of file with original text
+original_file_name = input("Enter the name of file: ")
+# New file with changed text
+new_file_name = input("Enter the name of new file: ")
 
+class SymbolChanger():
+    def __init__(self, base_char, new_char, original_file_name, new_file_name):
+        self.base_char = base_char
+        self.new_char = new_char
+        self.original_file_name = original_file_name
+        self.new_file_name = new_file_name
 
-char_changer()
+    def char_changer(self):
+        # Load orignal file
+        with open(original_file_name) as source_file:
+            lines = source_file.readlines()
+            # New file with changed text
+            with open(new_file_name, 'w') as output:
+                for line in lines:
+                    new_line = line.replace(base_char, new_char)
+                    output.write(new_line)
+
+new_SymbolChanger = SymbolChanger(base_char, new_char, original_file_name, new_file_name)
+new_SymbolChanger.char_changer()
